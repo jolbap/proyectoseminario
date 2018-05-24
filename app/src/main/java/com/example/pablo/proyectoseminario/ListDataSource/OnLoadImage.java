e423e808e7bc6002c0784c9c729d0fd647388f9d
pablo <olbapmus777@gmail.com>
2018-05-23 23:38:55 -0400

    creacion de login cliente enlace con cuenta google

---
+++ app/src/main/java/com/example/pablo/proyectoseminario/ListDataSource/OnLoadImage.java
@@ -0,0 +1,8 @@
+package com.example.pablo.proyectoseminario.ListDataSource;
+
+import android.graphics.Bitmap;
+import android.widget.ImageView;
+
+public interface OnLoadImage {
+    public void setLoadImage(ImageView container, Bitmap img);
+}
