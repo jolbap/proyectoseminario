3ec5447bb48e6ae8773c2bcb36c972cb013e80a8
pablo <olbapmus777@gmail.com>
2018-05-23 20:07:13 -0400

    empezando a crear fragments para VENTA, ALQUILER Y LOGIN

---
+++ app/src/main/java/com/example/pablo/proyectoseminario/FragmentLogin.java
@@ -0,0 +1,15 @@
+package com.example.pablo.proyectoseminario;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentLogin extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        return rootView;
+    }
+}
