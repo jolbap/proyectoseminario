ceaad03372eed26e7da3d9d70b2aabbe4a81a81a
pablo <olbapmus777@gmail.com>
2018-05-24 00:44:58 -0400

    registro de casas por el agente

--- app/src/main/java/com/example/pablo/proyectoseminario/Agente.java
+++ app/src/main/java/com/example/pablo/proyectoseminario/Agente.java
@@ -2,12 +2,45 @@ package com.example.pablo.proyectoseminario;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Agente extends AppCompatActivity {
 
+    EditText editTextid, editTextcanthabit, editTextcantbaños, editTextsuperficie, editTextprecio, editTextaño, editTextdescripcion;
+    Button buttonagregar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agente);
+        editTextid = findViewById(R.id.editTextid);
+        editTextcanthabit = findViewById(R.id.editTextcanthabit);
+        editTextcantbaños = findViewById(R.id.editTextcantbaños);
+        editTextsuperficie = findViewById(R.id.editTextsuperficie);
+        editTextprecio = findViewById(R.id.editTextprecio);
+        editTextaño = findViewById(R.id.editTextaño);
+        editTextdescripcion = findViewById(R.id.editTextdescripcion);
+        buttonagregar = findViewById(R.id.buttonagregar);
+
+        buttonagregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                consumirServicio();
+            }
+        });
+    }
+    public void consumirServicio(){
+
+        String id = editTextid.getText().toString();
+        String canthabit = editTextcanthabit.getText().toString();
+        String cantbaños = editTextcantbaños.getText().toString();
+        String superficie = editTextsuperficie.getText().toString();
+        String precio = editTextprecio.getText().toString();
+        String año = editTextaño.getText().toString();
+        String descripcion = editTextdescripcion.getText().toString();
+
+        ServicioTask servicioTask = new ServicioTask(this,"http://192.168.1.3:7777/api/v1.0/homes", id, canthabit, cantbaños, superficie, precio, año, descripcion);
+        servicioTask.execute();
     }
 }
---
+++ app/src/main/java/com/example/pablo/proyectoseminario/ServicioTask.java
@@ -0,0 +1,141 @@
+package com.example.pablo.proyectoseminario;
+
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+
+public class ServicioTask extends AsyncTask<Void, Void, String> {
+    private Context httpContext;
+    ProgressDialog progressDialog;
+    public String resultadoapi="";
+    public String linkrequestAPI="";
+    public String id = "";
+    public String canthabit = "";
+    public String cantbaños = "";
+    public String superficie = "";
+    public String precio = "";
+    public String año = "";
+    public String descripcion = "";
+
+    public ServicioTask(Context ctx, String linkAPI, String id, String canthabit, String cantbaños, String superficie, String precio, String año, String descripcion ){
+        this.httpContext = ctx;
+        this.linkrequestAPI = linkAPI;
+        this.id = id;
+        this.canthabit = canthabit;
+        this.cantbaños = cantbaños;
+        this.superficie = superficie;
+        this.precio = precio;
+        this.año = año;
+        this.descripcion = descripcion;
+    }
+    @Override
+    protected void onPreExecute(){
+        super.onPreExecute();
+        progressDialog = ProgressDialog.show(httpContext, "Procesando Solicitud", "por favor, espere");
+    }
+
+    @Override
+    protected String doInBackground(Void... params){
+        String result = null;
+        String wsURL = linkrequestAPI;
+        URL url = null;
+        try {
+
+            url = new URL(wsURL);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            JSONObject parametrosPost = new JSONObject();
+            parametrosPost.put("id",id);
+            parametrosPost.put("canthabit",canthabit);
+            parametrosPost.put("cantbaños",cantbaños);
+            parametrosPost.put("superficie",superficie);
+            parametrosPost.put("precio",precio);
+            parametrosPost.put("año",año);
+            parametrosPost.put("descripcion",descripcion);
+
+            urlConnection.setReadTimeout(15000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
+
+            OutputStream os = urlConnection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(parametrosPost));
+            writer.flush();
+            writer.close();
+            os.close();
+
+            int responseCode = urlConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK){
+                BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuffer ab = new StringBuffer("");
+                String linea = "";
+                while ((linea=in.readLine()) != null){
+                    ab.append(linea);
+                    break;
+                }
+                in.close();
+                result = ab.toString();
+            }
+            else {
+                result = new String("Error" + responseCode);
+            }
+
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (JSONException e){
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+
+    }
+
+    @Override
+    protected void onPostExecute(String s){
+        super.onPostExecute(s);
+        progressDialog.dismiss();
+        resultadoapi=s;
+        Toast.makeText(httpContext,resultadoapi,Toast.LENGTH_LONG).show();
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        Iterator<String> itr = params.keys();
+        while (itr.hasNext()){
+            String key = itr.next();
+            Object value = params.get(key);
+            if (first)
+                first = false;
+            else
+                result.append("&");
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+        }
+        return result.toString();
+    }
+}
+
