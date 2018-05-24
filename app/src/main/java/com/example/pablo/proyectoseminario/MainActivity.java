d9916ade3ebddf5ae588e85dcb39260672d43242
pablo <olbapmus777@gmail.com>
2018-05-23 21:38:37 -0400

    creacion de botones para CLIENTE Y AGENTE para logueo en android studio

--- app/src/main/java/com/example/pablo/proyectoseminario/MainActivity.java
+++ app/src/main/java/com/example/pablo/proyectoseminario/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pablo.proyectoseminario;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -66,6 +67,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    public void cliente(View v){
+        Intent inte = new Intent(this, LoginCliente.class);
+        startActivity(inte);
+    }
+    public void agente(View v){
+        Intent inte3 = new Intent(this, LoginAgente.class);
+        startActivity(inte3);
+    }
 
 
     @Override
@@ -90,40 +99,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_venta, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
